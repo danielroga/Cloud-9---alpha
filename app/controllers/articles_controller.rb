a620923e17212c00c181141bdab5d13da0cd866d
DRRG <daniel.rodriguez.gambetta85@gmail.com>
2017-02-04 02:36:32 +0000

    added delete and partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,16 @@
 class ArticlesController < ApplicationController
+    
+    def index
+        @article_list = Article.all
+    end
+    
+    
     def new
         @article = Article.new
     end
     
     def create
-        #render plain: params[:article].inspect     #test data
+        #render plain: params[:article].inspect     #test
         @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was successfully created"
@@ -13,9 +19,30 @@ class ArticlesController < ApplicationController
             render :new
         end
     end
+  
+    def edit
+        @article = Article.find(params[:id])
+    end
+  
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render :edit
+        end
+    end  
     
     def show
         @article = Article.find(params[:id])
+    end
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Articles was successfully deleted"
+        redirect_to articles_path
     end
     
     private
