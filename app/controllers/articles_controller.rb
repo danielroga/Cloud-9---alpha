0bdb4d17c1e8e566ba1fe7f2bbd30142728dba66
DRRG <daniel.rodriguez.gambetta85@gmail.com>
2017-02-04 02:58:40 +0000

    fixed show path

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was successfully created"
-            redirect_to article_show(@article)
+            redirect_to article_path(@article)
         else
             render :new
         end
