ccaffb03824fbb9ea303f00eb477fc03b0f9c96e
DRRG <daniel.rodriguez.gambetta85@gmail.com>
2017-02-09 03:45:37 +0000

    Style form partial

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,13 @@
 class Article < ActiveRecord::Base
-    validates :title, presence: true, length: {minimum: 5, maximum: 50}, format: { with: /\A[a-zA-Z0-9]*\z/, :message => 'no special characters, only letters and numbers'  }
-    validates :description, presence: true, length: {minimum:10, maximum: 300}, format: { with: /\A[a-zA-Z0-9]*\z/, :message => 'no special characters, only letters and numbers' }
+    validates :title,   presence: true, 
+                        length: {minimum: 5, maximum: 50}, 
+                        format: { 
+                            with: /\A[a-zA-Z0-9]*\z/, 
+                            :message => 'no special characters, only letters and numbers'  
+                        }
+    validates :description, presence: true, 
+                            length: {minimum:10, maximum: 300}, 
+                            format: { with: /\A[a-zA-Z0-9]*\z/, 
+                            :message => 'no special characters, only letters and numbers'
+                            }
 end
