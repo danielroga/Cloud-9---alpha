f308ad1b6dca8eb6f1dac029fc03b11a7db0b548
DRRG <daniel.rodriguez.gambetta85@gmail.com>
2017-02-16 05:24:53 +0000

    Add user validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,8 @@ class Article < ActiveRecord::Base
                         }
     validates :description, presence: true, 
                             length: {minimum:10, maximum: 300}, 
-                            format: { with: /\A[a-zA-Z0-9]*\z/, 
-                            :message => 'no special characters, only letters and numbers'
+                            format: { 
+                                with: /\A[a-zA-Z0-9]*\z/, 
+                                :message => 'no special characters, only letters and numbers'
                             }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+    validates :username,    presence: true,
+                            uniqueness: {case_sensitive: false}, #ensures uniques upper/lower case
+                            length: {minimum: 6, maximum: 15},
+                            format: { 
+                                with: /\A[a-zA-Z0-9]*\z/, 
+                                :message => 'Only Alphanumeric Characters'  
+                            }
+
+    validates :email,       presence: true,
+                            uniqueness: {  case_sensitive: false    }, #ensures uniques upper/lower case_sensitive
+                            length: {   maximum: 50 },
+                            format: {
+                                with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, #REGEX email validation
+                                :message => 'Not a Valid Email Address'
+                            }
 end
