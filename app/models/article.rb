bc9e4378af5d21cb65cc03b8a74ab990c2585abe
DRRG <daniel.rodriguez.gambetta85@gmail.com>
2017-02-27 02:29:37 +0000

    Add user data to articleslisting page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,16 +1,16 @@
 class Article < ActiveRecord::Base
     belongs_to :user
     validates :title,   presence: true, 
-                        length: {minimum: 5, maximum: 50}, 
-                        format: { 
-                            with: /\A[a-zA-Z0-9]*\z/, 
-                            :message => 'no special characters, only letters and numbers'  
-                        }
+                        length: {minimum: 5, maximum: 50} #, 
+    #                     format: { 
+    #                         with: /\A[a-zA-Z0-9]*\z/, 
+    #                         :message => 'no special characters, only letters and numbers'  
+    #                     }
     validates :description, presence: true, 
-                            length: {minimum:10, maximum: 300}, 
-                            format: { 
-                                with: /\A[a-zA-Z0-9]*\z/, 
-                                :message => 'no special characters, only letters and numbers'
-                            }
+                            length: {minimum:10, maximum: 300}#, 
+                            # format: { 
+                            #     with: /\A[a-zA-Z0-9]*\z/, 
+                            #     :message => 'no special characters, only letters and numbers'
+                            # }
     validates :user_id, presence: true
 end
